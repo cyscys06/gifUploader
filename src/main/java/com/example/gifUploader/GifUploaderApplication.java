05c9aea20ccb1244e83b52afafbc093016159eaf
cyscys06 <cyscys06@gmail.com>
2025-11-04 20:51:29 +0900

    gifUploader 프로젝트 생성

---
+++ src/main/java/com/example/gifUploader/GifUploaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.gifUploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifUploaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifUploaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gifUploader/GifUploaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gifUploader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GifUploaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
